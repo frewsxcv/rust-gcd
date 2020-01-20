a8aea9258debb713dd01653aa504cfaa66c4b4c0
Simon Auch <auchsimon@googlemail.com>
2020-01-20 15:16:42 +0100

    use pseudo random values for benchmarks

--- benches/gcd_benchmark.rs
+++ benches/gcd_benchmark.rs
@@ -1,24 +1,30 @@
 extern crate criterion;
 extern crate gcd;
+extern crate rand;
+extern crate rand_chacha;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use gcd::Gcd;
+use rand::{Rng, SeedableRng};
+
+const SEED: u64 = 314;
 
 fn criterion_benchmark(c: &mut Criterion) {
     macro_rules! bench_function {
         ($name:expr, $type:ty, $func:ident) => {
             c.bench_function($name, |b| {
+                let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(SEED);
+                let values: Vec<$type> = (0..1000).map(|_| rng.gen()).collect();
                 b.iter(|| {
-                    for i in 0..core::u8::MAX {
-                        for j in 0..core::u8::MAX {
-                            let (i, j) = (i as $type, j as $type);
-                            black_box(i.$func(j));
-                        }
+                    for i in 0..values.len() - 1 {
+                        let (a, b) = (values[i], values[i + 1]);
+                        black_box(a.$func(b));
                     }
                 })
             });
         };
     };
+
     bench_function!("gcd euclid u8", u8, gcd_euclid);
     bench_function!("gcd euclid u16", u16, gcd_euclid);
     bench_function!("gcd euclid u32", u32, gcd_euclid);
